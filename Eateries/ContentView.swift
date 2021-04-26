01a761f40830d143a818fcb747a06906b178dfdd
Johnson Taylor <johnson.taylor@griffithuni.edu.au>
2021-04-27 07:35:31 +1000

    Updates to base code, introducing persistence

--- Eateries/ContentView.swift
+++ Eateries/ContentView.swift
@@ -33,80 +33,128 @@ extension String {
 }
 
 struct ContentView: View {
-    // instating all Entries from ListView
-    @ObservedObject  var entries: ViewModel
+    @Binding var entries: [Entry]
     var body: some View {
         NavigationView {
-            MasterView(entries: entries)
-                .navigationBarTitle(Text("Favorite Foods"))
-                .navigationBarItems(leading:
-                    EditButton(), trailing:
-                    Button(action: {
-                    withAnimation {
-                        entries.addElement()
-                    }
-                }) {
-                        Image(systemName: "plus")
-                })
+            MasterView(entries: $entries)
+                .navigationBarItems(leading: EditButton(), trailing: Button("+", action: { entries.append(Entry(title: "New", location: "New", notes: "New", review: "New"))}))
         }
     }
 }
-
 
 struct MasterView: View {
-    @Environment(\.editMode) var editMode
-    @ObservedObject var entries: ViewModel
+    @Binding var entries: [Entry]
     var body: some View {
-            List {
-                ForEach(entries.model, id: \.description) { entry in
-                    NavigationLink(
-                        destination: DetailView(entry: entry), label: {
-                        HStack{
-                            Image(uiImage: entry.image.load())
-                                .resizable()
-                                .frame (width:75, height:50)
-                                .scaledToFit()
-                            VStack(alignment: .leading){
-                                
-                                
-                                Text(entry.title)
-                                    .padding(.horizontal, 10)
-                                    .padding(.vertical, 10)
-                                    .font(.system(size: 20))
-                                    
-                                
-                                Text(entry.description)
-                                    .multilineTextAlignment(.leading)
-                                    .padding(.horizontal, 10)
-                                    .padding(.vertical, 10)
-                                    .font(.footnote)
-                                    .lineSpacing(0.5)
-                                
-                            }
-                        }
-                    })
-                }.onMove(perform: move)
-                .onDelete {
-                    entries.remove(at: $0)
-                }
-                
-        
+        List {
+            ForEach(entries) {
+                DetailView(entry: $entries[identifiedBy: $0])
+            }.onMove {
+                entries.move(fromOffsets: $0, toOffset: $1)
+                EntryApp.save()
+            }.onDelete {
+                entries.remove(atOffsets: $0)
+                EntryApp.save()
+            }
         }
-        
-        
     }
-    func move(from source: IndexSet,to destination: Int) {
-        entries.model.move(fromOffsets: source, toOffset: destination)
-    }
-    
 }
-        
-            
-            
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView(entries:ViewModel())
-        }
+
+struct DetailView: View {
+    @Binding var entry: Entry
+    var body: some View {
+        TextField("Enter Entry name", text: $entry.title, onCommit:  {
+            EntryApp.save()
+        })
     }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(entries: Binding(get: {
+            EntryApp.model
+        }, set: { newValue in
+            EntryApp.model = newValue
+        }))
+    }
+}
 
 
+
+//struct ContentView: View {
+//    // instating all Entries from ListView
+//    @ObservedObject  var entries: ViewModel
+//    var body: some View {
+//        NavigationView {
+//            MasterView(entries: entries)
+//                .navigationBarTitle(Text("Favorite Foods"))
+//                .navigationBarItems(leading:
+//                    EditButton(), trailing:
+//                    Button(action: {
+//                    withAnimation {
+//                        entries.addElement()
+//                    }
+//                }) {
+//                        Image(systemName: "plus")
+//                })
+//        }
+//    }
+//}
+//
+//
+//struct MasterView: View {
+//    @Environment(\.editMode) var editMode
+//    @ObservedObject var entries: ViewModel
+//    var body: some View {
+//            List {
+//                ForEach(entries.model, id: \.description) { entry in
+//                    NavigationLink(
+//                        destination: DetailView(entry: entry), label: {
+//                        HStack{
+//                            Image(uiImage: entry.image.load())
+//                                .resizable()
+//                                .frame (width:75, height:50)
+//                                .scaledToFit()
+//                            VStack(alignment: .leading){
+//
+//
+//                                Text(entry.title)
+//                                    .padding(.horizontal, 10)
+//                                    .padding(.vertical, 10)
+//                                    .font(.system(size: 20))
+//
+//
+//                                Text(entry.description)
+//                                    .multilineTextAlignment(.leading)
+//                                    .padding(.horizontal, 10)
+//                                    .padding(.vertical, 10)
+//                                    .font(.footnote)
+//                                    .lineSpacing(0.5)
+//
+//                            }
+//                        }
+//                    })
+//                }.onMove(perform: move)
+//                .onDelete {
+//                    entries.remove(at: $0)
+//                }
+//
+//
+//        }
+//
+//
+//    }
+//    func move(from source: IndexSet,to destination: Int) {
+//        entries.model.move(fromOffsets: source, toOffset: destination)
+//    }
+//
+//}
+//
+//
+//
+//    struct ContentView_Previews: PreviewProvider {
+//        static var previews: some View {
+//            ContentView(entries:ViewModel())
+//        }
+//    }
+//
+//
--- Eateries/ViewModel.swift
+++ Eateries/ViewModel.swift
@@ -1,23 +1,23 @@
+////
+////  ViewModel.swift
+////  Milestone1
+////
+////  Created by Johnson Taylor on 8/4/21.
+////
 //
-//  ViewModel.swift
-//  Milestone1
+//import Foundation
+//import SwiftUI
 //
-//  Created by Johnson Taylor on 8/4/21.
-//
-
-import Foundation
-import SwiftUI
-
-class ViewModel: ObservableObject {
-    @Published var model = [Entry]()
-    
-    func addElement() {
-        let entryNumber = model.count + 1
-        let entry = Entry(title: "Title", description: "\(entryNumber)", story: "Enter story", image: "Enter Image URL", recipe: "Enter Recipe", ingredients: "Enter Ingredients", idNumber: "" )
-        model.append(entry)
-    }
-    
-    func remove(at indices: IndexSet) {
-        model.remove(atOffsets: indices)
-    }
-}
+//class ViewModel: ObservableObject {
+//    @Published var model = [Entry]()
+//    
+//    func addElement() {
+//        let entryNumber = model.count + 1
+//        let entry = Entry(title: "Title", description: "\(entryNumber)", story: "Enter story", image: "Enter Image URL", recipe: "Enter Recipe", ingredients: "Enter Ingredients", idNumber: "" )
+//        model.append(entry)
+//    }
+//    
+//    func remove(at indices: IndexSet) {
+//        model.remove(atOffsets: indices)
+//    }
+//}
