a9143e16bd7675f0bf27ee977b1de7cbd1fcf227
Johnson Taylor <johnson.taylor@griffithuni.edu.au>
2021-04-29 07:27:47 +1000

    Completed basic tests

--- EateriesTests/EateriesTests.swift
+++ EateriesTests/EateriesTests.swift
@@ -29,5 +29,73 @@ class EateriesTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testEntry() throws {
+        let title = "food"
+        let location = "yum"
+        let notes = "delicious"
+        let author = ["stuff"]
+        let review = ["step1"]
+        let image = "dish"
+        let entryTest = Entry(title: title, location: location, notes: notes, review: review, author: author, image: image)
+        XCTAssertEqual(entryTest.title, "food")
+        XCTAssertEqual(entryTest.location, "yum")
+        XCTAssertEqual(entryTest.notes, "delicious")
+        XCTAssertEqual(entryTest.review, ["step1"])
+        XCTAssertEqual(entryTest.author, ["stuff"])
+        XCTAssertEqual(entryTest.image, "dish")
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testTitle() throws {
+        let title = "food"
+        let entryTest = Entry(title: title, location: "location", notes: "notes", review: ["review"], author: ["author"], image: "image")
+        XCTAssertEqual(entryTest.title, "food")
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testLocation() throws {
+        let location = "food"
+        let entryTest = Entry(title: "title", location: location, notes: "notes", review: ["review"], author: ["author"], image: "image")
+        XCTAssertEqual(entryTest.location, "food")
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testNotes() throws {
+        let notes = "food"
+        let entryTest = Entry(title: "title", location: "location", notes: notes, review: ["review"], author: ["author"], image: "image")
+        XCTAssertEqual(entryTest.notes, "food")
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testAuthor() throws {
+        let author = ["food"]
+        let entryTest = Entry(title: "title", location: "location", notes: "notes", review: ["review"], author: author, image: "image")
+        XCTAssertEqual(entryTest.author, ["food"])
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testReview() throws {
+        let review = ["food"]
+        let entryTest = Entry(title: "title", location: "location", notes: "notes", review: review, author: ["author"], image: "image")
+        XCTAssertEqual(entryTest.review, ["food"])
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testImage() throws {
+        let image = "food"
+        let entryTest = Entry(title: "title", location: "location", notes: "notes", review: ["review"], author: ["author"], image: image)
+        XCTAssertEqual(entryTest.image, "food")
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+
 
 }
